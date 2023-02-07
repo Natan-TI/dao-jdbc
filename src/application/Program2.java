91cceee6ce487abab040722097a7824b2cd72f00
Natan-TI <natan201125@hotmail.com>
2023-02-07 12:45:23 -0300

    create DepartmentDaoJDBC and Program2

---
+++ src/application/Program2.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		DepartmentDao dp = DaoFactory.createDepartmentDao();
+		
+	}
+
+}
